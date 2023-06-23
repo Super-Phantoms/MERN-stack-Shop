67e749f99a5173359ccf03341c5013e454f25827
SuperPhantom <super.phantom.dev@gmail.com>
2023-06-23 14:42:27 -0700

    modified

--- client/app/app.js
+++ client/app/app.js
@@ -1,8 +1,3 @@
-/**
- *
- * app.js
- *
- */
 
 import React from 'react';
 import { Provider } from 'react-redux';
@@ -15,19 +10,10 @@ import Application from './containers/Application';
 import ScrollToTop from './scrollToTop';
 import setToken from './utils/token';
 
-// Import application sass styles
 import './styles/style.scss';
-
-// Import Font Awesome Icons Set
 import 'font-awesome/css/font-awesome.min.css';
-
-// Import Simple Line Icons Set
 import 'simple-line-icons/css/simple-line-icons.css';
-
-// react-bootstrap-table2 styles
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-
-// rc-slider style
 import 'rc-slider/assets/index.css';
 
 // Authentication
